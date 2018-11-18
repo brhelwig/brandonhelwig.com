1eca9b68dfe026b1da727685863420b28b1e2284
Brandon Helwig <git@brandonhelwig.com>
2018-11-18 03:39:18 -0700

    add live reload server

--- serve.sh
+++ serve.sh
@@ -1,1 +1,1 @@
-docker run --rm --volume="$PWD:/srv/jekyll" -p 4000:4000 -it jekyll/jekyll jekyll serve -l
+docker run --rm --volume="$PWD:/srv/jekyll" -p 4000:4000 -p 35729:35729 -it jekyll/jekyll jekyll serve -l
